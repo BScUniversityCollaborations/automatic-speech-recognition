31c68fbada794951d47c07d5350d79d7d8c2da68
Νικόλας Π <nikolas.98@live.com>
2021-07-10 17:35:59 +0300

    Bug fixes

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
+# Lang
 INPUT_FILE = "Enter sound file path:\n"
 FILE_NOT_FOUND = "File not found at:\n"
+FILE_WRONG_EXTENSION = "File at is not .mp3 or .wav:\n"
+
+# Variables
+TOP_DB = 40
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import librosa
 
 from constants import *
 
@@ -11,5 +12,12 @@ filePath = input(INPUT_FILE)
 if os.path.exists(filePath) is False:
     sys.exit(FILE_NOT_FOUND + str(filePath))
 
+# Check if file is mp3 or wav
+if filePath.endswith(".mp3") or filePath.endswith(".wav") or filePath.endswith(".m4a") is not True:
+    sys.exit(FILE_WRONG_EXTENSION + str(filePath))
 
+signal, sr = librosa.load(filePath)
+signal_trimmed, i = librosa.effects.trim(TOP_DB, signal)
+
+print(signal + "\n" + sr + "\n" + signal_trimmed + "\n" + i)
 
