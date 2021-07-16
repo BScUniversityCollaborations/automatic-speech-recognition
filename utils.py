0ebfc7021145657204d855ec8ef76f5cda435b3c
Νικόλας Π <nikolas.98@live.com>
2021-07-16 03:54:02 +0300

    Bug fixes & added more comments + organized code.

--- utils.py
+++ utils.py
@@ -11,6 +11,10 @@ from plots import *
 
 def pre_processing(signal_data, file_name):
     # === Pre-Emphasis ===
+    # Parameters:
+    #   signal_data: A nparray with the original signal.
+    #   file_name: A string that contains the file name.
+
     signal_emphasized = librosa.effects.preemphasis(signal_data)
 
     # === Filtering ===
@@ -52,14 +56,19 @@ def pre_processing(signal_data, file_name):
 
 
 def remove_noise(signal_data):
+    # Parameters:
+    #   signal_data: A nparray with the original signal.
+
     reduced_noise = nr.reduce_noise(audio_clip=signal_data,
-                                    noise_clip=signal_data,
-                                    verbose=False)
+                                    noise_clip=signal_data)
 
     return reduced_noise
 
 
 def calculate_short_time_energy(signal_data):
+    # Parameters:
+    #   signal_data: A nparray with the original signal.
+
     signal = np.array(signal_data, dtype=float)
     win = sg.get_window("hamming", 301)
 
@@ -73,17 +82,20 @@ def calculate_short_time_energy(signal_data):
 
 
 def digits_segmentation(signal_nparray):
+    # Parameters:
+    #   signal_data: A nparray with the filtered signal.
+
     # We reverse the signal nparray.
     signal_reverse = signal_nparray[::-1]
 
-    frames = librosa.onset.onset_detect(signal_nparray, sr=DEFAULT_SAMPLE_RATE, hop_length=FRAME_LENGTH, backtrack=True)
+    frames = librosa.onset.onset_detect(signal_nparray, sr=DEFAULT_SAMPLE_RATE, hop_length=FRAME_LENGTH)
     times = librosa.frames_to_time(frames, sr=DEFAULT_SAMPLE_RATE, hop_length=FRAME_LENGTH)
     samples = librosa.frames_to_samples(frames, FRAME_LENGTH)
 
-    frames_reverse = librosa.onset.onset_detect(signal_reverse, sr=DEFAULT_SAMPLE_RATE, hop_length=FRAME_LENGTH,
-                                                backtrack=True)
+    frames_reverse = librosa.onset.onset_detect(signal_reverse, sr=DEFAULT_SAMPLE_RATE, hop_length=FRAME_LENGTH)
     times_reverse = librosa.frames_to_time(frames_reverse, sr=DEFAULT_SAMPLE_RATE, hop_length=FRAME_LENGTH)
 
+    # todo change the variable i name to be more accurate
     i = 0
     while i < len(times_reverse) - 1:
         times_reverse[i] = WINDOW_LENGTH - times_reverse[i]
@@ -116,8 +128,11 @@ def digits_segmentation(signal_nparray):
 
 
 def digit_recognition(signal_data, samples):
-    i = 0
-    # number of valid digits from onset detection todo
+    # Parameters:
+    #   signal_data: An nparray with the signal.
+    #   samples: An ndarray that contains integers.
+
+    # todo change this number of valid digits from onset detection
     count_digits = 0
     digit = {}
 
@@ -134,30 +149,50 @@ def digit_recognition(signal_data, samples):
 
 
 def recognition(digits, signal_data, dataset):
+    # === Recognition of Digits ===
+    # Parameters:
+    #   digits: An array containing integer digits.
+    #   signal_data: A nparray with the original signal for comparison.
+    #   dataset: todo
+
     # Init an array that will contain our recognized digits in string.
     recognized_digits_array = []
     for digit in digits:
-        mfcc_input = librosa.feature.mfcc(digit, signal_data, hop_length=FRAME_LENGTH, n_mfcc=13)
+        cost_matrix_new = []
+        mfccs = []
+
+        #
+        mfcc_input = librosa.feature.mfcc(y=digit,
+                                          S=signal_data,
+                                          sr=DEFAULT_SAMPLE_RATE,
+                                          hop_length=FRAME_LENGTH,
+                                          n_mfcc=13)
         mfcc_input_mag = librosa.amplitude_to_db(abs(mfcc_input))
 
-        Dnew = []
-        mfccs = []
         # 0-9 from training set
         for i in range(len(dataset)):
             # We basically filter the training dataset as well.
             dataset[i] = filter_signal(dataset[i])
+
             # MFCC for each digit from the training set
-            mfcc = librosa.feature.mfcc(dataset[i], signal_data, hop_length=80, n_mfcc=13)
+            mfcc = librosa.feature.mfcc(y=dataset[i],
+                                        S=signal_data,
+                                        sr=DEFAULT_SAMPLE_RATE,
+                                        hop_length=80,
+                                        n_mfcc=13)
+
             # logarithm of the features ADDED
             mfcc_mag = librosa.amplitude_to_db(abs(mfcc))
+
             # apply dtw
-            D, wp = librosa.sequence.dtw(X=mfcc_input_mag, Y=mfcc_mag, metric='euclidean', backtrack=True)
+            cost_matrix, wp = librosa.sequence.dtw(X=mfcc_input_mag, Y=mfcc_mag)
+
             # make a list with minimum cost of each digit
-            Dnew.append(D[-1, -1])
+            cost_matrix_new.append(cost_matrix[-1, -1])
             mfccs.append(mfcc_mag)
 
         # index of MINIMUM COST
-        index = Dnew.index(min(Dnew))
+        index_min_cost = cost_matrix_new.index(min(cost_matrix_new))
 
         show_mel_spectrogram()
 
@@ -165,6 +200,7 @@ def recognition(digits, signal_data, dataset):
 
 
 def get_training_samples_signal():
+
     # Initialize an array to append the signals of the training samples.
     training_samples_signals = []
 
@@ -185,6 +221,9 @@ def get_training_samples_signal():
 
 def filter_signal(signal_data):
     # === Filtering ===
+    # Parameters:
+    #   signal_data: A nparray with the signal.
+
     # Remove the background noise from the audio file.
     signal_reduced_noise = remove_noise(signal_data)
 
@@ -199,27 +238,36 @@ def filter_signal(signal_data):
     return signal_filtered
 
 
-def cross_validation(labels, mfcc):
-    training, testing = train_test_split(labels, test_size=0.3, shuffle=True)
+def k_fold_cross_validation(labels, mfccs):
+    # === K-Fold Cross Validation of training ===
+    # Parameters:
+    #   labels: todo
+    #   mfccs: todo
 
-    D = np.ones((len(labels), len(labels))) * -1
+    # todo check if test size...
+    trainings, testings = train_test_split(labels, test_size=0.3, shuffle=True)
+
+    # Init a all-ones matrix with the same row & column length of labels (ex: 30x30).
+    all_ones_matrix = np.ones(
+        len(labels), len(labels)
+    ) * -1
 
     score = 0.0
-    for i in range(len(testing)):
-        x = mfcc[i]
-        dmin, jmin = math.inf, -1
+    for test in range(len(testings)):
+        x = mfccs[test]
+        d_min, j_min = math.inf, -1
 
-        for j in range(len(training)):
-            y = mfccs[j]
-            d = D[i, j]
+        for training in range(len(trainings)):
+            y = mfccs[training]
+            d = all_ones_matrix[test, training]
 
             if d.all() == -1:
                 d = librosa.sequence.dtw(x, y)
 
-            if d.all() < dmin:
-                dmin = d
-                jmin = j
+            if d.all() < d_min:
+                d_min = d
+                j_min = training
 
-        score += 1.0 if (labels[i] == labels[jmin]) else 0.0
+        score += 1.0 if (labels[test] == labels[j_min]) else 0.0
 
-    print('Rec rate {}%'.format(100. * score / len(testing)))
+    print('Rec rate {}%'.format(100. * score / len(testings)))
